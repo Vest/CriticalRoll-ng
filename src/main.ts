9d50d34295bb49ce68693ae0ff61f21fa38cc4b1
Vest <Vest@users.noreply.github.com>
2025-04-12 21:45:47 +0300

    Refactor main.ts for improved organization and readability
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
+import {provideHttpClient} from "@angular/common/http";
+import {importProvidersFrom} from "@angular/core";
 import {bootstrapApplication} from '@angular/platform-browser';
 import {PreloadAllModules, provideRouter, RouteReuseStrategy, withPreloading} from '@angular/router';
-import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 
-import {routes} from './app/app.routes';
-import {AppComponent} from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
+import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 import {IonicStorageModule} from "@ionic/storage-angular";
-import {importProvidersFrom} from "@angular/core";
+
+import {AppComponent} from './app/app.component';
+import {routes} from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
     provideIonicAngular(),
     provideHttpClient(),
     importProvidersFrom(IonicStorageModule.forRoot()),
